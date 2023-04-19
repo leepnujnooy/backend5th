db17ab8d3fbfb288f5cc419e727938a86bdd3d64
Yoon Jun Pil <galaxy160102@gmail.com>
2023-04-19 13:11:24 +0900

    Feat: 마지막으로 Crawl 시간 작업1 -CrawlDate 도메인 추가

---
+++ src/main/java/com/example/javaproject2/CrawlDate.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2;
+
+public class CrawlDate {
+
+    public class CrawlDate {
+        private LocalDateTime lastCrawlDateTime;
+        // alt + insert
+
+        public CrawlDate(LocalDateTime lastCrawlDateTime) {
+            this.lastCrawlDateTime = lastCrawlDateTime;
+        }
+    }
+
+
+}
+//checkout 개념은 브랜치이동.
