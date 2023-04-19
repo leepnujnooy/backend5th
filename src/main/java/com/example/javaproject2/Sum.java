69fc96912d422de3ef4f536a5a2fd4e483b06e75
Yoon Jun Pil <galaxy160102@gmail.com>
2023-04-19 11:39:49 +0900

    -Sum 기능 추가

---
+++ src/main/java/com/example/javaproject2/Sum.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2;
+
+public class Sum {
+    public int returnNum(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/main/java/com/example/javaproject2/SumTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2;
+
+public class SumTest {
+    public static void main(String[] args) {
+        Sum sumtest = new Sum();
+        System.out.println(sumtest.returnNum(5,5));
+    }
+}
