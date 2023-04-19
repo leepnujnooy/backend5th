78eee195d22251e204a7b08e65269b3baad49575
Yoon Jun Pil <galaxy160102@gmail.com>
2023-04-19 15:33:12 +0900

    week 1 : Scanner class practice

---
+++ src/main/java/com/example/javaproject2/week01/ScannerExampleTest.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week01;
+
+import java.util.Scanner;
+
+public class ScannerExampleTest {
+    public static void main(String[] args) {
+        ScannerExample se = new ScannerExample();
+        se.readTwoNumAndPlus();
+    }
+
+}
