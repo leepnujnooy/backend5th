dfa84f6b808d49ebc0c4e0faad031e12a462aba8
Yoon Jun Pil <galaxy160102@gmail.com>
2023-04-19 14:47:11 +0900

    week 1 : BufferedReader class exercise

---
+++ src/main/java/com/example/javaproject2/week01/BufferTest.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class BufferTest {
+    public static void main(String[] args) throws IOException {
+
+        Buffer bf = new Buffer();
+
+        bf.readALine();
+
+
+    }
+}
