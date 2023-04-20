cb747822e0b32555baa256ba6499cc4b7a49cbfb
Yoon Jun Pil <galaxy160102@gmail.com>
2023-04-20 11:32:04 +0900

    week 1 : 코드업 1010~1014 풀이 추가

---
+++ src/main/java/com/example/javaproject2/codeUp/codeUp1013.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeUp;
+
+import java.util.Scanner;
+
+public class codeUp1013 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next() + " " + sc.next();
+        System.out.println(n);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeUp/codeUp1014.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeUp;
+
+import java.util.Scanner;
+
+public class codeUp1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st1 = sc.next();
+        String st2 = sc.next();
+
+        System.out.println(st2 + " " + st1);
+    }
+}
