5aefebf992a701c01d885780c9a98b82597fd17c
Yoon Jun Pil <galaxy160102@gmail.com>
2023-04-21 10:08:47 +0900

    week 1 : 변수와 스캐너

---
+++ src/main/java/com/example/javaproject2/week01/MemoryTest.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week01;
+
+public class MemoryTest {
+
+    public void print(){
+        System.out.println();
+
+    }
+    public static void main(String[] args) {
+        int mainNum = 10;
+        MemoryTest mt = new MemoryTest();
+        mt.print();
+    }
+}
