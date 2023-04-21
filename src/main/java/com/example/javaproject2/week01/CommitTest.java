df343d4587fd793fbd13f51cb04bcf6f76c555f5
Yoon Jun Pil <galaxy160102@gmail.com>
2023-04-21 10:20:28 +0900

    Commit Test 파일 수정 ..

--- src/main/java/com/example/javaproject2/week01/CommitTest.java
+++ src/main/java/com/example/javaproject2/week01/CommitTest.java
@@ -2,6 +2,6 @@ package com.example.javaproject2.week01;
 
 public class CommitTest {
     public static void main(String[] args) {
-        System.out.println("New File Created");
+        System.out.println("New File Created 2");
     }
 }
