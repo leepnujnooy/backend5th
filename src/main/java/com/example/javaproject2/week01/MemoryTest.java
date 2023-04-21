fbccda582c3ff91eaf5e34856373dcfcf7ac3bd4
Yoon Jun Pil <galaxy160102@gmail.com>
2023-04-21 10:15:23 +0900

    Memory 파일 수정

--- src/main/java/com/example/javaproject2/week01/MemoryTest.java
+++ src/main/java/com/example/javaproject2/week01/MemoryTest.java
@@ -3,11 +3,11 @@ package com.example.javaproject2.week01;
 public class MemoryTest {
 
     public void print(){
-        System.out.println();
+        System.out.println("Memory : 000");
 
     }
     public static void main(String[] args) {
-        int mainNum = 10;
+
         MemoryTest mt = new MemoryTest();
         mt.print();
     }
