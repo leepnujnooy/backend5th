addb1b821a6a2812a3c76d689b7ae65099e6811d
Yoon Jun Pil <galaxy160102@gmail.com>
2023-04-28 09:21:50 +0900

    2week/5day : If 실습

---
+++ src/main/java/com/example/javaproject2/week02/day05/ElseEx.java
@@ -0,0 +1,23 @@
+package com.example.javaproject2.week02.day05;
+
+public class ElseEx {
+    public static void main(String[] args) {
+        boolean checkTrue = true;
+        if(checkTrue){
+            System.out.println("T");
+        }
+        else{
+            System.out.println("F");
+        }
+
+        boolean checkFalse = false;
+        if(checkFalse){
+            System.out.println("F");
+        }
+        else{
+            System.out.println("T");
+        }
+
+
+    }
+}
