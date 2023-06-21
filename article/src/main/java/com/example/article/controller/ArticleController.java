9fa5ef98ae75f686725cd2926282f38efb99d510
Yoon Jun Pil <galaxy160102@gmail.com>
2023-06-21 14:50:49 +0900

    파일 수정 및 로직 추가

--- article/src/main/java/com/example/article/controller/ArticleController.java
+++ article/src/main/java/com/example/article/controller/ArticleController.java
@@ -1,29 +1,57 @@
 package com.example.article.controller;
 
+import com.example.article.dto.ArticleDto;
 import com.example.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Slf4j
-@Controller
+@RestController // @ResponseBody 생략
+@RequestMapping("/articles")
 @RequiredArgsConstructor
 public class ArticleController {
     private final ArticleService service;
 
     // POST /articles
+    @PostMapping
+    public ArticleDto create(@RequestBody ArticleDto dto){
+        return service.createArticle(dto);
+    }
+
+
 
 
     // GET /articles
+    @GetMapping
+    public List<ArticleDto> readAll(){
+        return service.readArticleAll();
+    }
+
 
 
     // GET /articles/{id}
+    @GetMapping("/{id}")
+    public ArticleDto read(@PathVariable("id") Long id){
+        return service.readArticle(id);
+    }
+
 
 
     // PUT /articles/{id}
+    @PutMapping("/{id}")
+    public ArticleDto update(@PathVariable("id") Long id , @RequestBody ArticleDto dto){
+        return service.updateArticle(id,dto);
+    }
 
 
     // DELETE /articles/{id}
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id")Long id){
+        service.deleteArticle(id);
+    }
 
 }
--- article/src/main/java/com/example/article/service/ArticleService.java
+++ article/src/main/java/com/example/article/service/ArticleService.java
@@ -1,13 +1,16 @@
 package com.example.article.service;
 
 import com.example.article.dto.ArticleDto;
+import com.example.article.entity.ArticleEntity;
 import com.example.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,22 +18,70 @@ public class ArticleService {
     private final ArticleRepository repository;
 
     public ArticleDto createArticle(ArticleDto dto) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+
+        ArticleEntity articleEntity = new ArticleEntity();
+
+        articleEntity.setTitle(dto.getTitle());
+        articleEntity.setContent(dto.getContent());
+        articleEntity.setWriter(dto.getWriter());
+
+        repository.save(articleEntity);
+
+        return dto;
     }
 
     public ArticleDto readArticle(Long id) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+
+        Optional<ArticleEntity> optionalArticleEntity = repository.findById(id);
+
+        if(optionalArticleEntity.isPresent()){
+            return ArticleDto.fromEntity(optionalArticleEntity.get());
+        }
+        else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
     public List<ArticleDto> readArticleAll() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+        List<ArticleEntity> articleEntityList = repository.findAll();
+        List<ArticleDto> articleDtoList = new ArrayList<>();
+
+        for (ArticleEntity articleEntity : articleEntityList){
+            articleDtoList.add(ArticleDto.fromEntity(articleEntity));
+        }
+
+        return articleDtoList;
     }
 
     public ArticleDto updateArticle(Long id, ArticleDto dto) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+        Optional<ArticleEntity> optionalArticleEntity = repository.findById(id);
+
+        if(optionalArticleEntity.isPresent()){
+            ArticleEntity articleEntity = optionalArticleEntity.get();
+            articleEntity.setTitle(dto.getTitle());
+            articleEntity.setContent(dto.getContent());
+            articleEntity.setWriter(dto.getWriter());
+            repository.save(articleEntity);
+
+            return ArticleDto.fromEntity(articleEntity);
+        }
+        else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+
+
     }
 
     public void deleteArticle(Long id) {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+        Optional<ArticleEntity> optionalArticle = repository.findById(id);
+
+        if(optionalArticle.isPresent()){
+            repository.delete(optionalArticle.get());
+        }
+        else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
     }
 }
